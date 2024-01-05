fec633e61664e16f3dc3ee4f7e07408bdc0edf27
Tac Tacelosky <tacman@gmail.com>
2024-01-04 20:15:09 -0600

    use flysystem for writing

--- src/MessageHandler/FetchWikidataMessageHandler.php
+++ src/MessageHandler/FetchWikidataMessageHandler.php
@@ -5,6 +5,8 @@ namespace App\MessageHandler;
 use App\Entity\Official;
 use App\Message\FetchWikidataMessage;
 use Doctrine\ORM\EntityManagerInterface;
+use League\Flysystem\FilesystemOperator;
+use Psr\Log\LoggerInterface;
 use Survos\WikiBundle\Service\WikiService;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
@@ -20,6 +22,8 @@ final class FetchWikidataMessageHandler
         private WikiService $wikiService,
         private EntityManagerInterface $entityManager,
         private HttpClientInterface $httpClient,
+        private FilesystemOperator $defaultStorage,
+        private LoggerInterface $logger,
         #[Autowire('%kernel.project_dir%')] private string $projectDir,
     )
     {
@@ -39,26 +43,31 @@ final class FetchWikidataMessageHandler
         foreach ($values->getIterator() as $item) {
             // we could do this in an async message, too.
             $url = $item->id;
-            $downloadedFileLocation = sprintf('%s/public/images/%s/', $this->projectDir, $wikidataId);
-            // this will fail on production, unless /public/images is linked to persistent storage
-            if (!is_dir($downloadedFileLocation)) {
-                try {
-                    mkdir($downloadedFileLocation, recursive: true);
-                } catch (\Exception $exception) {
-                    dd($downloadedFileLocation, $exception);
-                }
-            }
+//            $downloadedFileLocation = sprintf('%s/public/images/%s/', $this->projectDir, $wikidataId);
+//            // this will fail on production, unless /public/images is linked to persistent storage
+//            if (!is_dir($downloadedFileLocation)) {
+//                try {
+//                    mkdir($downloadedFileLocation, recursive: true);
+//                } catch (\Exception $exception) {
+//                    dd($downloadedFileLocation, $exception);
+//                }
+//            }
             $code = md5($url) . '.' . pathinfo($url, PATHINFO_EXTENSION);
-            $path = $downloadedFileLocation . $code;
-            if (!file_exists($path)) {
+//            $path = $downloadedFileLocation . $code;
+            if (!$this->defaultStorage->has($code)) {
+                $this->logger->warning("Fetching $url");
                 $request = $this->httpClient->request('GET', $url, [
 //                    'User-Agent' => 'SurvosBot/0.1 (tacman@gmail.com) generic-library/0.0'
                 ]);
                 if ($request->getStatusCode() == 200) {
-                    file_put_contents($path, $request->getContent());
+                    $this->defaultStorage->write($code, $request->getContent());
+
+//                    file_put_contents($path, $request->getContent());
                 } else {
                     dd($request);
                 }
+            } else {
+                $this->logger->warning("$code already exists");
             }
             $images[] = $code;
 
