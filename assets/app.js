cc8b9db5f62cdaeb3a069586f7deaf3f749ff61d
Tac Tacelosky <tacman@gmail.com>
2025-06-26 06:34:38 -0400

    update recipe

--- assets/app.js
+++ assets/app.js
@@ -4,7 +4,7 @@
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css'
+import './styles/app.css';
 
 // stimulus
 import './bootstrap.js';
