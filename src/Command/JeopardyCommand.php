eff8676b003d552e90701da3484b01f170b02244
Tac Tacelosky <tacman@gmail.com>
2025-06-29 11:23:46 -0400

    add a progress bar

--- src/Command/JeopardyCommand.php
+++ src/Command/JeopardyCommand.php
@@ -8,6 +8,7 @@ use Symfony\Component\Console\Attribute\Argument;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Attribute\Option;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\ObjectMapper\ObjectMapper;
@@ -38,25 +39,30 @@ class JeopardyCommand
 
         $mapper = new ObjectMapper();
 //        $filename = 'data/jeopardy.json';
-        $filename = 'data/sample.json';
+//        $filename = 'data/sample.json';
         // $product = new Product();
         // $manager->persist($product);
-        foreach (json_decode(file_get_contents($filename)) as $idx => $value) {
+        $data = json_decode(file_get_contents($filename));
+        $progressBar = new ProgressBar($io, count($data));
+        foreach ($data as $idx => $value) {
+            $progressBar->advance();
             $entity = $mapper->map($value, Jeopardy::class);
             $this->entityManager->persist($entity);
 ////            }
-            if ( ($idx) % ($batch-1) === 0) {
+            if ( $progressBar->getProgress() % ($batch-1) === 0) {
                 try {
                     $this->entityManager->flush();
                 } catch (\Exception $e) {
                     dd($value, $idx, $e->getMessage());
                 }
-                dump($entity, idx: $idx);
+                if ($io->isVerbose()) {
+                    dump($entity, idx: $idx);
+                }
                 $this->entityManager->clear();
-                break;
             }
         }
         $this->entityManager->flush();
+        $progressBar->finish();
 
 		$io->success(self::class . " success.");
 		return Command::SUCCESS;
